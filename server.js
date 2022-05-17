7ec98fa29787eb047280a0254c0bae5c5c8748cd
Jessie Zollinger <jzolly@hotmail.com>
2022-05-17 19:21:47 -0400

    we got drinks

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // required dependencies
 const express = require('express');
-
+const drinks =require('./models/drinks');
 // initialize the express application
 const app = express();
-
+// port
+const port = 3000;
 // mount our routes;
 app.get('/', (req, res) => {
-    res.send('')
+    res.send('Welcome to the Gitpub App!')
+});
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
 });
 // this tells node to listen for requests is running on: localhost:3000
 app.listen(3000, () => {
