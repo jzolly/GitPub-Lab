2c8bceaf365e5e983527fe8e53498aabc9d23a17
Jessie Zollinger <jzolly@hotmail.com>
2022-05-17 20:35:37 -0400

    added route to drinks_show

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // required dependencies
 const express = require('express');
 const drinks =require('./models/drinks');
-// this is the function to pass all arrays through for capitalizing first letter
+// this is the function to pass all arrays through for capitalizing first letter, will work for all arrays.
 function capitalizeWords(arr) {
     return arr.map(element => {
         // make sure to reassign the variable to this new to uppcase function.
@@ -26,6 +26,14 @@ app.get('/', (req, res) => {
 });
 app.get('/drinks', (req, res) => {
     res.render('drinks_index.ejs', { 'allDrinks': drinks })
+});
+app.get('/drinks/:id', (req, res) => {
+    res.send(req.params.id)
+});
+app.get('/drinks/:indexOfDrinksArray', (req, res) => {
+    res.render('drinks_show.ejs', {
+        drink: drinks[req.params.indexOfDrinksArray]
+    });
 });
 // this tells node to listen for requests is running on: localhost:3000
 app.listen(3000, () => {
